82de6187fa69f9808f45b21f95f2958c966f3d15
JohanCampos <43768539+JohanCampos@users.noreply.github.com>
2023-07-20 12:59:48 -0600

    Agregando texturas y nav mesh

---
+++ Assets/BuscaRutas.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BuscaRutas : MonoBehaviour
+{
+    public GameObject Objetivo;
+    public NavMeshAgent[] agent;
+
+    public GameObject carpetaDeAgentes;
+    int numeroDeAgentes;
+
+
+    void Start()
+    {
+        numeroDeAgentes = carpetaDeAgentes.transform.childCount;
+
+        agent = new NavMeshAgent[numeroDeAgentes];
+
+        for (int i = 0; i < numeroDeAgentes; i++)
+        {
+            agent[i] = carpetaDeAgentes.transform.GetChild(i).gameObject.GetComponent<NavMeshAgent>();
+        }
+    }
+
+    void Update()
+    {
+       
+
+    }
+}
